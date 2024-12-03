a55c7c9b8697a3fafa69f7007aa2a77a5bc4531f
jallal-msd <jallalblack@gmail.com>
2024-12-03 22:00:15 +0100

    day1 part1

---
+++ day1/day1_p1.lua
@@ -0,0 +1,43 @@
+file = io.open("input1.txt")
+
+io.input(file)
+
+res = 0
+words = {}
+left = {}
+right = {}
+for str in file:lines() do
+	for word in string.gmatch(str, "%S+") do
+		table.insert(words, word)
+	end
+end
+
+for i, v in ipairs(words) do
+	if i == 1 or i % 2 ~= 0 then
+		table.insert(left, v)
+	end
+	if i == 2 or i % 2 == 0 then
+		table.insert(right, v)
+	end
+end
+table.sort(left)
+table.sort(right)
+
+for i, v in ipairs(left) do
+	if left[i] <= right[i] then
+		diff = right[i] - left[i]
+	else
+		diff = left[i] - right[i]
+	end
+	res = res + diff
+end
+
+print(res)
+-- for i, v in ipairs(left) do
+-- 	print(v)
+-- end
+-- print("-----------------")
+--
+-- for i, v in ipairs(right) do
+-- 	print(v)
+-- end
